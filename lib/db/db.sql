1f87fbf0106a85bc263732a8714429b5b78be819
zhou xiaojiang <zhouxiaojiang@startdt.com>
2018-03-14 15:51:08 +0800

    fix: log

--- lib/db/db.sql
+++ lib/db/db.sql
@@ -6,16 +6,17 @@ CREATE TABLE `api_monitor` (
   `url` varchar(500) DEFAULT NULL COMMENT '接口地址',
   `response` text COMMENT '服务端响应数据',
   `param` text COMMENT '前端传递参数',
-  `type` int(5) DEFAULT NULL COMMENT '错误类型：0：未知错误；1、服务端系统错误；2、用户非正常操作导致的错误（例如表单输入不合法字段前端未过滤掉，服务端返回的错误）；3：接口超时；4、返回非json字段（抛java异常代码）',
+  `type` int(5) DEFAULT NULL COMMENT '错误类型：0：未知错误；1、数据响应非成功接口；2、ajax触发error；4、返回非json字段（抛java异常代码）',
   `desc` text COMMENT '错误详细描述',
   `group` int(20) DEFAULT NULL COMMENT '部门：1、无人零售；2、智能零售；3、魔方大数据；4、官网中台；5、安卓app',
   `app` varchar(1000) DEFAULT NULL COMMENT '应用名称：例如（酒窖项目）',
   `ua` varchar(500) DEFAULT NULL COMMENT '用户的ua信息',
-  `referer` varchar(1000) DEFAULT NULL COMMENT '接口的referer信息',
+  `href` varchar(1000) DEFAULT NULL COMMENT '调用接口的页面地址',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '时间',
   `others` text COMMENT '额外想记录的数据',
+  `status` int(2) NOT NULL DEFAULT '1' COMMENT '记录状态：1、有效的；0、无效的',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=208 DEFAULT CHARSET=utf8 COMMENT='异常接口监控表';
+) ENGINE=InnoDB AUTO_INCREMENT=261 DEFAULT CHARSET=utf8 COMMENT='异常接口监控表';
 
 
 
